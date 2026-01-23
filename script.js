54e23b14cde84b4b78f6e28d964c2cea808dab77
LOKESH <codelearning2846@gmail.com>
2026-01-23 20:04:14 +0530

    my own site

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-// BURGER MENU
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav-links');
+// Typing Effect
+const text = ["Creative Developer", "Frontend Engineer", "Problem Solver"];
+let i = 0, j = 0;
+const typing = document.querySelector(".typing");
 
-burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
-    burger.classList.toggle('toggle');
-});
+function type() {
+    if (j < text[i].length) {
+        typing.innerHTML += text[i][j];
+        j++;
+        setTimeout(type, 100);
+    } else {
+        setTimeout(erase, 1500);
+    }
+}
 
-// CONTACT FORM
-const contactForm = document.getElementById('contactForm');
-contactForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert("Message sent successfully!");
-    contactForm.reset();
+function erase() {
+    if (j > 0) {
+        typing.innerHTML = text[i].substring(0, j - 1);
+        j--;
+        setTimeout(erase, 50);
+    } else {
+        i = (i + 1) % text.length;
+        setTimeout(type, 500);
+    }
+}
+type();
+
+// Custom Cursor
+const cursor = document.querySelector(".cursor");
+document.addEventListener("mousemove", e => {
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
 });
